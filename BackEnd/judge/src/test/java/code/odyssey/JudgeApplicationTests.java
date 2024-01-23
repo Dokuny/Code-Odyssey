41e89ebdf4e1c70d264baf0e778640a64a7734d9
the_1_week <the_1_week@naver.com>
2024-01-23 16:50:44 +0900

    [Back-Judge] feat: Judge서버 모듈 생성

---
+++ BackEnd/judge/src/test/java/code/odyssey/JudgeApplicationTests.java
@@ -0,0 +1,13 @@
+package code.odyssey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
