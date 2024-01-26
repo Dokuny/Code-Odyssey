64fb59ffcb06e8c982eb313427d7f4160bcc4182
이도훈 <nestour95@gmail.com>
2024-01-26 13:29:25 +0900

    [Back] add guild-sprint creation API

--- BackEnd/common/src/main/java/code/odyssey/common/domain/guildSprint/entity/GuildSprint.java
+++ BackEnd/common/src/main/java/code/odyssey/common/domain/guildSprint/entity/GuildSprint.java
@@ -1,25 +1,25 @@
 package code.odyssey.common.domain.guildSprint.entity;
 
 import code.odyssey.common.domain.guild.entity.Guild;
+import code.odyssey.common.domain.guildSprint.entity.enums.GuildSprintStatus;
+import code.odyssey.common.domain.guildSprint.exception.GuildSprintException;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Builder.Default;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static code.odyssey.common.domain.guildSprint.entity.enums.GuildSprintStatus.WAITING;
 import static jakarta.persistence.CascadeType.ALL;
+import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder @Getter
+@NoArgsConstructor(access = PROTECTED)
+@Getter
 @Entity
 public class GuildSprint {
 
@@ -33,12 +33,37 @@ public class GuildSprint {
     private Guild guild;
 
     @Column
+    private String title;
+
+    @Column
+    private Integer period;
+
+    @Column
+    @Enumerated(STRING)
+    private GuildSprintStatus status;
+
+    @Column
     private LocalDate startedAt;
 
     @Column
     private LocalDate endedAt;
 
-    @Default
-    @OneToMany(mappedBy = "sprint", cascade = ALL, orphanRemoval = true)
-    private List<GuildSprintSchedule> schedules = new ArrayList<>();
+    @OneToMany(mappedBy = "guildSprint", orphanRemoval = true, cascade = ALL)
+    private List<GuildSprintProblem> problems = new ArrayList<>();
+
+    public static GuildSprint createGuildSprint(Guild guild, String title, Integer period) {
+        GuildSprint guildSprint = new GuildSprint();
+
+        guildSprint.guild = guild;
+
+        if (period < 1 || period > 7) {
+            throw new IllegalArgumentException("스프린트 기간은 1~7일 사이여야 합니다.");
+        }
+
+        guildSprint.title = title;
+        guildSprint.period = period;
+        guildSprint.status = WAITING;
+
+        return guildSprint;
+    }
 }
---
+++ BackEnd/common/src/main/java/code/odyssey/common/domain/guildSprint/exception/GuildSprintErrorCode.java
@@ -0,0 +1,22 @@
+package code.odyssey.common.domain.guildSprint.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@AllArgsConstructor
+public enum GuildSprintErrorCode {
+	SPRINT_IN_PROGRESS("진행중인 스프린트가 존재합니다.",BAD_REQUEST),
+	NOT_EXISTS_GUILD("길드를 찾을 수 없습니다.",BAD_REQUEST),
+	NO_AUTHNETICATION("권한이 없습니다.", FORBIDDEN),
+	NOT_EXISTS_PROBLEM("문제를 찾을 수 없습니다.", BAD_REQUEST)
+	;
+
+	private final String message;
+	private final HttpStatus httpStatus;
+
+
+}
