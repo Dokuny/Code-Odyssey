9ce3b41caadad34521686a01f4b41a18a3f5512a
이주현 <jennysophia3@naver.com>
2024-02-02 00:24:12 +0900

    [Back] feat: 개인 스프린트 조회 API

--- BackEnd/common/src/main/java/code/odyssey/common/domain/memberSprint/controller/MemberSprintController.java
+++ BackEnd/common/src/main/java/code/odyssey/common/domain/memberSprint/controller/MemberSprintController.java
@@ -1,6 +1,7 @@
 package code.odyssey.common.domain.memberSprint.controller;
 
 import code.odyssey.common.domain.memberSprint.dto.MemberSprintCreateRequest;
+import code.odyssey.common.domain.memberSprint.dto.MemberSprintInfo;
 import code.odyssey.common.domain.memberSprint.service.MemberSprintService;
 import code.odyssey.common.domain.problem.entity.Problem;
 import lombok.RequiredArgsConstructor;
@@ -16,18 +17,27 @@ public class MemberSprintController {
 
     private final MemberSprintService memberSprintService;
 
-    // 스프린트 스케줄 생성
-    @PostMapping("/schedule")
-    public ResponseEntity<Void> createSprintSchedule(
+    // 스프린트 생성
+    @PostMapping
+    public ResponseEntity<Void> createSprint(
             @RequestHeader("X-Authorization-Id") Long memberId,
             @RequestBody MemberSprintCreateRequest request
     ) {
-        memberSprintService.createSprintSchedule(memberId, request);
+        memberSprintService.createSprint(memberId, request);
         return ResponseEntity.ok().build();
     }
 
-    // 스프린트 오늘의 추천 문제 보여주기
+    // 스프린트 조회
     @GetMapping
+    public ResponseEntity<List<MemberSprintInfo>> getSprint(
+            @RequestHeader("X-Authorization-Id") Long memberId
+    ) {
+        return ResponseEntity.ok(memberSprintService.getSprint(memberId));
+    }
+
+
+    // 스프린트 오늘의 추천 문제 보여주기
+    @GetMapping("/recommend")
     public ResponseEntity<List<Problem>> getRecommendedProblems(
             @RequestHeader("X-Authorization-Id") Long memberId
     ) {
