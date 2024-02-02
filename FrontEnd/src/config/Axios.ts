39dc7bd17ca9e37b200338d7b8ef6d3d76ad7d33
김한주 <the_1_week@naver.com>
2024-02-02 09:50:12 +0900

    [Infra]fix: axios 요청 운영환경 url 변경

--- FrontEnd/src/config/Axios.ts
+++ FrontEnd/src/config/Axios.ts
@@ -1,3 +1,3 @@
-export const BASE_URL: string = 'http://localhost:8888';
+export const BASE_URL: string = 'http://3.35.176.227:8888';
 export const TIMEOUT: number = 1500;
 export const CONTENT_TYPE: string = 'application/json; charset=utf-8';
