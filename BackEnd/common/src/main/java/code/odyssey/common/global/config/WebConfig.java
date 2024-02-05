9f5a9227af47e81813df5a78d390fcb1714abef2
the_1_week <the_1_week@naver.com>
2024-02-05 11:21:30 +0900

    [Infra]fix: cors 해제

--- BackEnd/common/src/main/java/code/odyssey/common/global/config/WebConfig.java
+++ BackEnd/common/src/main/java/code/odyssey/common/global/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins("*")
+				.allowedOriginPatterns("*")
 				.allowedMethods(
 						HttpMethod.GET.name(),
 						HttpMethod.POST.name(),
