79dd887bbdae090c9a9e8f2eeddcee2a9ccbd6e4
the_1_week <the_1_week@naver.com>
2024-02-05 13:24:30 +0900

    [Infra]fix: cors 설정 변경

---
+++ BackEnd/common/src/main/java/code/odyssey/common/global/config/WebConfig.java
@@ -0,0 +1,35 @@
+package code.odyssey.common.global.config;
+
+import code.odyssey.common.global.oauth.vendor.enums.OAuthDomainConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${front.url}")
+    private String frontUrl;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new OAuthDomainConverter());
+    }
+
+
+}
+
