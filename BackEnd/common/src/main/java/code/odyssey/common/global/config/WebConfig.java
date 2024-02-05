d4b3300835410373b79d5cfa5845225818b36e16
the_1_week <the_1_week@naver.com>
2024-02-05 11:51:12 +0900

    [Infra]fix: cors허용 메서드변경

--- BackEnd/common/src/main/java/code/odyssey/common/global/config/WebConfig.java
+++ BackEnd/common/src/main/java/code/odyssey/common/global/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOriginPatterns("http://3.35.176.227:8888/")
+				.allowedOrigins("http://3.35.176.227:8888/")
 				.allowedMethods(
 						HttpMethod.GET.name(),
 						HttpMethod.POST.name(),
