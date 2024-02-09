0928c7bac50aa6779bce331c3a9b10cb61b19a0a
김수린 <uhyeon1009@gmail.com>
2024-02-09 16:11:00 +0900

    [Front] feat: GuildMember 보는 페이지 axios 누락 추락

--- FrontEnd/src/components/organisms/guild/main/GuildProfile/GuildPerson.tsx
+++ FrontEnd/src/components/organisms/guild/main/GuildProfile/GuildPerson.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getGuildMembers } from '../../../../../utils/api/guild/setting/guildsetting';
 import { Spacer } from '../../../../atoms/basic/Spacer';
 import MemberInfoCard from '../../../../molecules/card/basic/MemberInfoCard';
 
@@ -7,18 +8,17 @@ interface GuildPersonProps {
 }
 
 const GuildPerson = (props: GuildPersonProps) => {
-  const [data, setData] = useState([
-    { member_id: 1, name: 'name1', difficulty: 2, join_at: '2024-01-31', thumbnail: 'https://picsum.photos/300' },
-    { member_id: 4, name: 'name4', difficulty: 1, join_at: '2024-01-31', thumbnail: 'https://picsum.photos/300' },
-    { member_id: 3, name: 'name3', difficulty: 4, join_at: '2024-01-31', thumbnail: 'https://picsum.photos/300' },
-    { member_id: 2, name: 'name2', difficulty: 3, join_at: '2024-01-31', thumbnail: 'https://picsum.photos/300' },
-    { member_id: 5, name: 'name5', difficulty: 5, join_at: '2024-01-31', thumbnail: 'https://picsum.photos/300' },
-    { member_id: 7, name: 'name7', difficulty: 5, join_at: '2024-01-31', thumbnail: 'https://picsum.photos/300' },
-    { member_id: 6, name: 'name6', difficulty: 10, join_at: '2024-01-31', thumbnail: 'https://picsum.photos/300' },
-    { member_id: 9, name: 'name9', difficulty: 25, join_at: '2024-01-31', thumbnail: 'https://picsum.photos/300' },
-    { member_id: 10, name: 'name10', difficulty: 15, join_at: '2024-01-31', thumbnail: 'https://picsum.photos/300' },
-    { member_id: 8, name: 'name8', difficulty: 2, join_at: '2024-01-31', thumbnail: 'https://picsum.photos/300' },
-  ]);
+  const [data, setData] = useState<Array<any>>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getGuildMembers(props.guild_id);
+      console.log(data);
+      setData(data);
+    };
+
+    fetchData();
+  }, [props.guild_id]);
 
   return (
     <>
