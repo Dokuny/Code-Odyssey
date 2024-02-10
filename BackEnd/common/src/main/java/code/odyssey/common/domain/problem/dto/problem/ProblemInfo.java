1b913fcc03597e7d9e41b72f02e6bce4a44e1639
dev1week <rondo2860@gmail.com>
2024-02-10 16:12:13 +0900

    [Back]feat: 문제 목록 검색 기능 개발

--- BackEnd/common/src/main/java/code/odyssey/common/domain/problem/dto/problem/ProblemInfo.java
+++ BackEnd/common/src/main/java/code/odyssey/common/domain/problem/dto/problem/ProblemInfo.java
@@ -2,11 +2,21 @@ package code.odyssey.common.domain.problem.dto.problem;
 
 import code.odyssey.common.domain.problem.entity.enums.ProblemPlatform;
 import code.odyssey.common.domain.problem.entity.enums.ProblemType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
-public record ProblemInfo(String title, ProblemPlatform platform, int difficulty, ProblemType type) {
+@Getter
+public class ProblemInfo {
 
-
+    private long problem_id;
+    private String title;
+    private ProblemPlatform platform;
+    private int difficulty;
+    private ProblemType type;
 
 }
