cde98059dee5aaef00f92d7479c1b7abc70a1852
dev1week <rondo2860@gmail.com>
2024-02-11 18:11:25 +0900

    [BackJudge]hotfix: cors해결

--- BackEnd/judge/src/main/java/code/odyssey/domain/judge/config/WebConfig.java
+++ BackEnd/judge/src/main/java/code/odyssey/domain/judge/config/WebConfig.java
@@ -1,3 +1,8 @@
+package code.odyssey.domain.judge.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
