f7ec5476a17760c170d4fddc0e340322c94c89af
Dohun Lee <nestour95@gmail.com>
2024-02-14 21:52:59 +0900

    [Back] hotfix: 길드원이 가득찰 시, 가입 및 검색 방지

--- BackEnd/common/src/main/java/code/odyssey/common/domain/guild/dto/GuildSearchInfo.java
+++ BackEnd/common/src/main/java/code/odyssey/common/domain/guild/dto/GuildSearchInfo.java
@@ -3,10 +3,15 @@ package code.odyssey.common.domain.guild.dto;
 import code.odyssey.common.domain.guild.entity.enums.LanguageType;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @JsonNaming(SnakeCaseStrategy.class)
+@Builder @AllArgsConstructor
+@NoArgsConstructor
 @Getter @Setter
 public class GuildSearchInfo {
 
--- BackEnd/common/src/main/java/code/odyssey/common/domain/guild/repository/GuildMemberRepository.java
+++ BackEnd/common/src/main/java/code/odyssey/common/domain/guild/repository/GuildMemberRepository.java
@@ -12,6 +12,9 @@ public interface GuildMemberRepository extends JpaRepository<GuildMember, Long>,
     @Query("select count(gm.id) from GuildMember gm where gm.member.id = :memberId and gm.resigned_at is null")
     Long countJoinGuild(@Param("memberId") Long memberId);
 
+    @Query("select count(gm.id) from GuildMember gm where gm.guild.id = :guildId and gm.resigned_at is null")
+    Long countGuildMembers(@Param("guildId") Long guildId);
+
     @Query("select gm from GuildMember gm join fetch gm.member join fetch gm.guild where gm.guild.id = :guildId")
     List<GuildMember> findAllByGuildId(@Param("guildId") Long guildId);
 }
