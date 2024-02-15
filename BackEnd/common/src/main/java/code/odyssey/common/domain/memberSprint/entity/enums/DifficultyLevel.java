af0b565169bf727e144bcd98a7a09fa18303780a
이주현 <jennysophia3@naver.com>
2024-02-15 13:59:51 +0900

    [Back] fix: 개인 스프린트 등록 디폴트 RANDOM으로 수정

--- BackEnd/common/src/main/java/code/odyssey/common/domain/memberSprint/entity/enums/DifficultyLevel.java
+++ BackEnd/common/src/main/java/code/odyssey/common/domain/memberSprint/entity/enums/DifficultyLevel.java
@@ -1,5 +1,5 @@
 package code.odyssey.common.domain.memberSprint.entity.enums;
 
 public enum DifficultyLevel {
-    BRONZE, SILVER, GOLD, PLATINUM, DIAMOND, RUBY
+    BRONZE, SILVER, GOLD, PLATINUM, DIAMOND, RUBY, RANDOM
 }
